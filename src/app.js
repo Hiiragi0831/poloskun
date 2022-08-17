a22d333cc0d39f7ed4117dc44b52600f58bddff4
hir01 <glazunovden@yandex.ru>
2022-08-17 21:50:40 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -11,13 +11,13 @@ if (document.getElementById('phone-mask')) {
       mask: '+{7}(000)-000-00-00'
     });
 }
-if (document.querySelector(".ferula-block__slider")) {
-  new Swiper(".ferula-block__slider .swiper", {
+if (document.querySelector('.ferula-block__slider')) {
+  new Swiper('.ferula-block__slider .swiper', {
     slidesPerView: 4,
     spaceBetween: 18,
     navigation: {
-      nextEl: ".ferula-block__slider-button-next",
-      prevEl: ".ferula-block__slider-button-prev",
+      nextEl: '.ferula-block__slider-button-next',
+      prevEl: '.ferula-block__slider-button-prev',
     },
     breakpoints: {
       540: {
@@ -34,7 +34,7 @@ if (document.querySelector(".ferula-block__slider")) {
 
 if (document.querySelector(".steps-block-slider__content")) {
 
-  let swiper = new Swiper(".steps-block-slider__content .swiper", {
+  let swiper = new Swiper('.steps-block-slider__content .swiper', {
     slidesPerView: 1,
     effect: "fade",
   });
@@ -42,9 +42,18 @@ if (document.querySelector(".steps-block-slider__content")) {
   const navItem = document.querySelectorAll('.steps-block-slider__nav-item');
   navItem.forEach(item => {
     item.addEventListener('click', function () {
-      document.querySelector('.steps-block-slider__nav-item.is-active').classList.remove('is-active');
       swiper.slideTo(item.dataset.id);
-      item.classList.add('is-active');
+    });
+  });
+
+  swiper.on('slideChangeTransitionEnd', function () {
+    document.querySelector('.steps-block-slider__nav-item.is-active').classList.remove('is-active');
+    const getActiveId = document.querySelector('.swiper-slide-active').dataset.slide;
+    navItem.forEach(item => {
+      if (item.dataset.id === getActiveId) {
+        console.log(item.dataset.id)
+        item.classList.add('is-active');
+      }
     });
   });
 }
